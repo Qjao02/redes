2dd1f3e6789bb3c3482459a14fe39b2f8acb99f1
juaumpc <joao_cruz02@hotmail.com>
2018-04-06 14:33:12 -0300

    Initial commit

---
+++ cliente.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# Coded by: Alisson Machado
+# Contact: alisson.machado@responsus.com.br
+#
+
+def formate (url):
+    url = client_socket
+import socket 
+ip = '10.0.0.248'
+port = 7000 
+addr = ((ip,port)) 
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+client_socket.connect(addr)
+mensagem = input() 
+client_socket.send(mensagem.encode('utf-8')) 
+print ('mensagem enviada') 
+client_socket.close()
