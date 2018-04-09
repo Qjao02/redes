64e6ce5d9ab4ce2ee4b17d4a7dce3ce1afeebedc
juaumpc <joao_cruz02@hotmail.com>
2018-04-09 09:16:59 -0300

    adcionado os scripts em python
    os scripts atuais se encontravam na forma de notebook

--- Servidor.py
+++ Servidor.py
@@ -12,14 +12,16 @@ import datetime as dt
 import threading as th
 
     
-def requestMessage (con,cliente):
+def requestMessage (con,cliente,extensionDict):
+    
     print ("aguardando mensagem") 
-    mensagem = con.recv(1024).decode('utf-8')
+    mensagem = con.recv(1048576).decode('utf-8')
 
-    msg = mensagem.split("\r\n")
+    msg = mensagem.split("\n")
     request['operation'] = msg[0]
+    
     del(msg[0])
-
+    
     print(request['operation'])
 
     #debug variable
@@ -34,21 +36,22 @@ def requestMessage (con,cliente):
             valor = lineSplit[1]
             request[key] = valor
         except:
-            print('error create dictionary on ' + str(cont) + 'line')
+            break
 
-
-    filepath = request['operation'].split()[1]
-
-    if request['operation'].split()[1]== '/':
-        print('if')
+    print(request['operation'].split())
+    try:
+        filepath = request['operation'].split()[1]
+    except:
+        filepath='servConfig/400.html'
+        
+    if filepath == '/':
         nameFile = request['operation'].split()
 
-        print (filepath)
         file = open('content/Index.html','rb')
 
         fileByte = file.read()
 
-        respostaString = '\nHTTP/1.1 200 Ok \n'
+        respostaString = '\nHTTP/1.1 200 Ok \r\n'
 
         resposta = {
             "Location" : "http://localhost:7000/",
@@ -61,71 +64,81 @@ def requestMessage (con,cliente):
         for key,valor in resposta.items():
             respostaString = respostaString + key+': '+ valor + '\r\n'
 
-
+        respostaString = respostaString + '\r\n'
         con.send( respostaString.encode('utf-8') + fileByte )
 
     else:
-        filePath = request['operation'].split()[1]
-        print('content' + filePath)
-        
+            
         if os.path.isfile('content' + filepath) :
             file = open('content'+filepath,'rb')
-            respostaString = '\nHTTP/1.1 200 ok! \n'
+            respostaString = '\nHTTP/1.1 200 ok! \r\n'
             fileByte = file.read()
+            index = filepath.rfind('.')
+            keyExtension = filepath[index:]
             
-        elif os.path.isdir('content' + filePath):
-            
+        elif os.path.isdir('content' + filepath):
             
             files = os.listdir('content' + filepath)
-            createListHtml(filePath,files)
+            createListHtml(filepath,files)
             
+            keyExtension = '.isdir'
             
             file = open('content/temp/listDir.html','rb')
             fileByte = file.read()
             respostaString = '\nHTTP/1.1 200 ok! \n'
-
+            
             
         else:
-            print('arquivo nao existe')
-            file = open('content/404.html','rb')
-            respostaString = '\nHTTP/1.1 404 Not Found! \n'
+            file = open('servConfig/404.html','rb')
+            respostaString = '\nHTTP/1.1 404 Not Found! \r\n'
             fileByte = file.read()
-
+            keyExtension = '.html'
 
         resposta = {
             "Location" : "http://localhost:7000/",
             'date' : str(dt.datetime.now()),
             'Server' : 'jaoserver',
-            'Content-Type' : 'text/html',
+            'Content-Type' : extensionDict[keyExtension],
             'Content-Length' : str(len(fileByte))
 
         }
+        print(resposta)
 
         for key,valor in resposta.items():
             respostaString = respostaString + key+': '+ valor + '\r\n'
         
-        respostaString = respostaString + '\n'
+        respostaString = respostaString + '\r\n'
+        print(respostaString)
         file.close()
-        con.send( respostaString.encode('utf-8') + fileByte)
+        con.sendall( respostaString.encode('utf-8') + fileByte )
     con.close()
 
 def createListHtml(filePath,files):
     file = open('content/temp/listDir.html','w')
-    file.write('<html> \r\n')
-    file.write('<head><title>listDir</title></head>\n')
-    file.write('<body>\n')
+    file.write('<html>')
+    file.write('<head><title>listDir</title></head>')
+    file.write('<body>')
     file.write('<h1>MUTHERFUCKER PAGES</H1>')
     for fileName in files:
-        file.writelines('<a href="' + filePath + '/'+fileName+'">'+fileName+'</a><br>\n')
+        file.write('<a href="' + filePath + '/'+fileName+'">'+fileName+'</a><br>')
     
-    file.writelines('</body>\n')
-    file.writelines('</html>\n')
+    file.write('</body>')
+    file.write('</html>')
     file.close()
     
 request = {}
-host = 'localhost' 
+host = '10.0.0.248' 
 port =  7000
 
+loadextensions = open('servConfig/extension.txt','r')
+extensionDict = {}
+
+for line in loadextensions:
+    keyValue = line.split('\t')
+    index = keyValue[1].find('\r\n')
+    extensionDict[keyValue[0]] = keyValue[1][:index]
+    
+loadextensions.close()
 addr = (host, port) 
 serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv_socket.bind(addr) 
@@ -134,16 +147,19 @@ serv_socket.listen(10)
 #variaveis declaradas
 file = ''
 fileByte = ''
-
-while True:
-    
-    print ('aguardando conexao') 
-    
+cons = set()
+cont = 0
+while True:    
     con, cliente = serv_socket.accept() 
     print ('conectado')
-    t = th.Thread(target = requestMessage, args=(con,cliente))
-    t.start()
-    t.join()
+    cons.add(con)
+    th.Thread(target=requestMessage,args=(con, cliente, extensionDict)).start()
+    print('numero de threads criadas = ' + str(cont))
     
-    
+
+
+# In[ ]:
+
+
+ldsplit
 
--- cliente.py
+++ cliente.py
@@ -1,17 +1,182 @@
-#!/usr/bin/python
-# Coded by: Alisson Machado
-# Contact: alisson.machado@responsus.com.br
-#
 
-def formate (url):
-    url = client_socket
-import socket 
-ip = '10.0.0.248'
-port = 7000 
-addr = ((ip,port)) 
+# coding: utf-8
+
+# In[ ]:
+
+
+import socket
+
+url = input()
+
+extensionFile = open('servConfig/extension.txt','r')
+extensionDict = {}
+for line in extensionFile:
+    keyValue = line.split()
+    extensionDict[keyValue[1]] = keyValue[0]
+    
+
+
+# In[ ]:
+
+
+if url.find('/') < 0:
+    url = url + '/'
+    
+
+
+# In[ ]:
+
+
+indexOf = url.find('/')
+host = url[:indexOf]
+getOperation = url[indexOf:]
+
+
+# In[ ]:
+
+
+fileExtension = ''
+
+
+# In[ ]:
+
+
+
+if(getOperation == '/'):
+    fileExtension = '.html'
+
+    
+else:
+    indexOfPoint = getOperation.rfind('.')
+    if (indexOfPoint < 0):
+        fileExtension = '.html'
+    else:
+        fileExtension = getOperation[indexOfPoint:]
+
+operation = 'GET ' + getOperation+ ' HTTP/1.1\r\n'
+
+request = {
+    'Host: ' : host,
+    'Connection: ' : 'keep-alive',
+    'Upgrade-Insecure-Requests: ' : '1',
+    'User-Agent: ' : 'jaoClient',
+    'Acept-Encoding: ' : 'gzir, deflate, br',
+    'Accept-Language: ' : 'en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7',
+}
+
+
+# In[ ]:
+
+
+HOST = host.split(':')[0]
+try :
+    PORT = host.split(':')[1]
+except:
+    PORT = 80
+    
+addr = ((HOST,int(PORT)))
+
+
+# In[ ]:
+
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 client_socket.connect(addr)
-mensagem = input() 
-client_socket.send(mensagem.encode('utf-8')) 
-print ('mensagem enviada') 
-client_socket.close()
+
+
+# In[ ]:
+
+
+stringRequest = operation
+for k,v in request.items():
+    stringRequest = stringRequest+ k + v + '\n'
+
+
+# In[ ]:
+
+
+client_socket.send(stringRequest.encode()) 
+
+
+# In[ ]:
+
+
+total_data=[]
+while True:
+    data = client_socket.recv(8192)
+    if not data: break
+    total_data.append(data)
+response = b''.join(total_data)
+
+
+# In[ ]:
+
+
+header = response.decode(errors="ignore")
+header = header[:header.rfind('\r\n\r\n')]
+
+
+# In[ ]:
+
+
+headerSplit = header.split('\r\n')
+status = headerSplit[0]
+del(headerSplit[0])
+print(status)
+
+headerDict = {}
+for line in headerSplit:
+    keyValue = line.split(': ')
+    try:
+        headerDict[keyValue[0]] = keyValue[1]
+    except:
+        break
+
+
+# In[ ]:
+
+
+saveBinFile = response[len(header)+4:]
+
+
+# In[ ]:
+
+
+if getOperation == '/':
+    fileName= 'Index'
+else:
+    indexOfLBar = getOperation.rfind('/')
+    fileName = getOperation[indexOfLBar:indexOfPoint]
+    
+
+
+# In[ ]:
+
+
+downloadPath = 'downloads/' + fileName + fileExtension
+
+
+# In[ ]:
+
+
+fileName
+
+
+# In[ ]:
+
+
+if status.split()[1] == '200':
+    download = None 
+    if headerDict['Content-Type'] != 'text/html':
+        download = open(downloadPath,'wb')
+        download.write(saveBinFile)
+        download.close()
+    else:
+        download = open(downloadPath,'w')
+        decodeFile = saveBinFile.decode()
+        download.write(decodeFile)
+        download.close()
+else:
+    print('erro ao processar a requisição do arquivo')
+   
+
